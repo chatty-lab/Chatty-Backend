8e95ee7bc4194a46783815b5ee77ad3eb5da5dc1
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-12-09 12:27:03 +0900

    feat: User Entity 구현

---
+++ src/main/java/com/chatty/domain/Role.java
@@ -0,0 +1,6 @@
+package com.chatty.domain;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/chatty/domain/entity/User.java
+++ src/main/java/com/chatty/domain/entity/User.java
@@ -1,16 +1,24 @@
 package com.chatty.domain.entity;
 
+import com.chatty.domain.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.geo.Point;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
@@ -18,13 +26,16 @@ import org.springframework.data.geo.Point;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
+    private String email;
+    private String mobileNumber;
+    private String password;
     private String nickname;
     private LocalDate birth;
     private String mbti;
@@ -32,5 +43,37 @@ public class User {
     private String address;
     private String uuid;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
 
