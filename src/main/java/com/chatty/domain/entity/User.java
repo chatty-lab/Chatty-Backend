8f529d724de2dbea5cd9e1459c590a6fc3104ff9
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-12-09 11:06:32 +0900

    chore: database, database-platform 제거

--- src/main/java/com/chatty/domain/entity/User.java
+++ src/main/java/com/chatty/domain/entity/User.java
@@ -2,31 +2,35 @@ package com.chatty.domain.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.geo.Point;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
-
     private String nickname;
-
     private LocalDate birth;
-
     private String mbti;
-
     private Point location;
-
     private String address;
-
     private String uuid;
+
 }
 
