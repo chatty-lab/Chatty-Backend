135346a5b3c91cf2256783926324bf5a38926e36
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-12-15 22:26:22 +0900

    [BE-5] feat: redis server 2개로 분리, redistemplate 방식으로 변경

--- src/main/java/com/chatty/config/RedisConfig.java
+++ src/main/java/com/chatty/config/RedisConfig.java
@@ -5,18 +5,50 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("localhost")
+    @Value("${redis-host}")
     private String host;
 
-    @Value("6379")
-    private int port;
+    @Value("${redis-token-port}")
+    private int tokenPort;
+
+    @Value("${redis-authenticationNumber-port}")
+    private int authenticationNumberPort;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory redisConnectionFactoryToken(){
+        return new LettuceConnectionFactory(host, tokenPort);
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactoryAuthenticationNumber(){
+        return new LettuceConnectionFactory(host,authenticationNumberPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(){
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactoryToken());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplateAuthNumber(){
+        RedisTemplate<String,String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactoryAuthenticationNumber());
+
+        return redisTemplate;
     }
 }
--- src/main/java/com/chatty/repository/RefreshTokenRepository.java
+++ src/main/java/com/chatty/repository/RefreshTokenRepository.java
@@ -1,11 +1,42 @@
 package com.chatty.repository;
 
-import com.chatty.entity.RefreshToken;
-import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken,Long> {
-    Optional<RefreshToken> findRefreshTokenByUuid(String uuid);
+public class RefreshTokenRepository {
+    private RedisTemplate<String, String> redisTemplateToken;
+
+    @Autowired
+    public RefreshTokenRepository(@Qualifier("redisTemplate") RedisTemplate<String, String> redisTemplateToken){
+        this.redisTemplateToken = redisTemplateToken;
+    }
+
+    public void save(String uuid, String refreshToken){
+        try {
+            ValueOperations<String, String> value = redisTemplateToken.opsForValue();
+            value.set(uuid,refreshToken);
+        }catch(Exception e) {
+            log.error("[RedistTokenService/getRefreshTokenByUuid] 데이터 저장 실패");
+        }
+    }
+
+    public String findRefreshTokenByUuid(String uuid){
+        try {
+            ValueOperations<String, String> value = redisTemplateToken.opsForValue();
+            return value.get(uuid);
+        }catch(Exception e) {
+            log.error("[RedistTokenService/getRefreshTokenByUuid] 일치하는 refresh 토큰이 존재하지 않습니다.");
+            return null;
+        }
+    }
+
+    public void delete(String uuid){
+        redisTemplateToken.delete(uuid);
+    }
 }
