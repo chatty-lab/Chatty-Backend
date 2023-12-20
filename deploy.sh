199da286d74b301ac310915ba44a713e4bbd611b
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-21 03:00:19 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 APP_NAME=chatty-api
 cd /home/ubuntu/chatty
+echo "> 실행 중인 Spring Boot 서비스 중지"
+docker compose down springBoot
 echo "> 도커 이미지 빌드"
 docker build -t "${APP_NAME}:latest" .
 echo "> 도커 컴포즈 실행"
