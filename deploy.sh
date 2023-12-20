3f23139a1187d83632286e020510567f3177d3d6
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-21 03:02:20 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker compose down springBoot
 echo "> 도커 이미지 빌드"
 docker build -t "${APP_NAME}:latest" .
 echo "> 도커 컴포즈 실행"
-docker compose up
+docker compose up -d
 echo "> 사용되지 않는 도커 이미지 삭제"
 docker image prune -f
 exit 0
