f36e7dde110d15f9217b77e9a19bc9b59569b547
HUNHIE LEE <huny98.dev@gmail.com>
2023-12-21 02:52:31 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/chatty
 echo "> 도커 이미지 빌드"
 docker build -t "${APP_NAME}:latest" .
 echo "> 도커 컴포즈 실행"
-docker compose up -d
+docker compose up
 echo "> 사용되지 않는 도커 이미지 삭제"
 docker image prune -f
 exit 0
