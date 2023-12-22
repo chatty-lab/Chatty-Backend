f24ca1a9ee0ef49f94964afb3435a96110cdf531
YunByungil <dbsquddlfz@naver.com>
2023-12-22 22:48:24 +0900

    [BE-35] test: 닉네임 변경하는 기능 테스트 완료

---
+++ src/test/java/com/chatty/controller/user/UserControllerTest.java
@@ -0,0 +1,104 @@
+package com.chatty.controller.user;
+
+import com.chatty.dto.user.request.UserNicknameRequest;
+import com.chatty.entity.user.Authority;
+import com.chatty.service.user.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken("01012345678", "", List.of()));
+    }
+
+    @DisplayName("회원가입시 닉네임을 등록한다.")
+    @Test
+    void updateNickname() throws Exception {
+        // given
+        UserNicknameRequest request = UserNicknameRequest.builder()
+                .nickname("닉넴")
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        put("/users/nickname").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("회원가입시 닉네임을 등록할 때, 닉네임은 필수 값이다.")
+    @Test
+    void updateNicknameWithoutNickname() throws Exception {
+        // given
+        UserNicknameRequest request = UserNicknameRequest.builder()
+//                .nickname("닉네임")
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        put("/users/nickname").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("닉네임은 필수로 입력해야 합니다."));
+    }
+
+    @DisplayName("회원가입시 닉네임을 등록할 때, 닉네임의 길이는 2~10자로 입력해야 한다.")
+    @CsvSource({"닉", "닉네임11글자입니다요", "띄 어 쓰 기"})
+    @ParameterizedTest
+    void updateNicknameWithNicknameSize(final String nickname) throws Exception {
+        // given
+        UserNicknameRequest request = UserNicknameRequest.builder()
+                .nickname(nickname)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        put("/users/nickname").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("닉네임은 특수문자를 제외한 2~10자리여야 합니다."));
+    }
+}
---
+++ src/test/java/com/chatty/service/user/UserServiceTest.java
@@ -0,0 +1,84 @@
+package com.chatty.service.user;
+
+import com.chatty.dto.user.request.UserNicknameRequest;
+import com.chatty.dto.user.response.UserResponse;
+import com.chatty.entity.user.Authority;
+import com.chatty.entity.user.User;
+import com.chatty.exception.CustomException;
+import com.chatty.repository.user.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("유저 닉네임을 수정한다.")
+    @Test
+    void updateNickname() {
+        // given
+        User user = createUser("닉네임", "01012345678");
+        User savedUser = userRepository.save(user);
+
+        UserNicknameRequest request = UserNicknameRequest.builder()
+                .nickname("바뀐닉네임")
+                .build();
+
+        // when
+        UserResponse userResponse = userService.updateNickname(savedUser.getMobileNumber(), request);
+
+        // then
+        assertThat(userResponse.getId()).isNotNull();
+        assertThat(userResponse.getNickname()).isEqualTo("바뀐닉네임");
+    }
+
+    @DisplayName("유저 닉네임을 수정할 때, 중복된 닉네임이 존재하면 예외가 발생한다.")
+    @Test
+    void updateNicknameWithDuplicateNickname() {
+        // given
+        User user = createUser("존재하는닉네임", "01012345678");
+        User newUser = createUser("닉네임", "01087654321");
+        userRepository.saveAll(List.of(user, newUser));
+
+        UserNicknameRequest request = UserNicknameRequest.builder()
+                .nickname("존재하는닉네임")
+                .build();
+
+        // when // then
+        assertThatThrownBy(() -> userService.updateNickname(newUser.getMobileNumber(), request))
+                .isInstanceOf(CustomException.class)
+                .hasMessage("이미 존재하는 닉네임 입니다.");
+    }
+
+    private User createUser(final String nickname, final String mobileNumber) {
+        return User.builder()
+                .mobileNumber(mobileNumber)
+                .uuid("123456")
+                .authority(Authority.USER)
+                .mbti("mbti")
+                .birth(LocalDate.now())
+                .imageUrl("이미지")
+                .address("주소")
+                .gender("남자")
+                .nickname(nickname)
+                .build();
+    }
+}
