c0892978ac0f337ce7c3d47502e77e3d835176f7
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-12-27 23:31:30 +0900

    [BE-32] feat: accessToken 검증 로직 오류 해결

---
+++ src/main/java/com/chatty/controller/message/MessageController.java
@@ -0,0 +1,9 @@
+package com.chatty.controller.message;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MessageController {
+}
