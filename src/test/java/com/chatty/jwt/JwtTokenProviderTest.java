32c030911d6e82968f2f270c8c9dc284cb0e0ce9
박상윤 <96441638+sangyunpark99@users.noreply.github.com>
2023-12-28 00:48:54 +0900

    Update JwtTokenProviderTest.java

--- src/test/java/com/chatty/jwt/JwtTokenProviderTest.java
+++ src/test/java/com/chatty/jwt/JwtTokenProviderTest.java
@@ -14,6 +14,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.util.ReflectionTestUtils;
+import com.chatty.utils.JwtTokenUtils;
 
 @SpringBootTest
 class JwtTokenProviderTest {
