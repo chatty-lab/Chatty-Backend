76925e0dce5ad4963b69cd1ec8c7878bd3c4b98d
YunByungil <dbsquddlfz@naver.com>
2024-01-05 18:27:58 +0900

    [BE-33] feat: 거리 계산 함수 메서드 추가

--- src/main/java/com/chatty/repository/user/UserRepository.java
+++ src/main/java/com/chatty/repository/user/UserRepository.java
@@ -1,8 +1,14 @@
 package com.chatty.repository.user;
 
 import com.chatty.entity.user.User;
+
+import java.util.List;
 import java.util.Optional;
+
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,4 +21,16 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsUserByUuid(String uuid);
 
     Optional<User> findByNickname(String nickname);
+
+    @Query(value = "select u.*, ST_Distance_Sphere(u.location, :point) as dis " +
+            "from Users u " +
+            "having dis <= :scope", nativeQuery = true)
+    List<User> customFindByDistance(@Param("point") Point point,
+                                    @Param("scope") Double scope);
+
+    @Query(value = "select ST_Distance_Sphere(:point, :point2) as dis " +
+            "having dis <= :scope", nativeQuery = true)
+    Double customFindByDistance2(@Param("point") Point point,
+                                    @Param("point2") Point point2,
+                                    @Param("scope") Double scope);
 }
