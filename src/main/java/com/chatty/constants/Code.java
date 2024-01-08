d1acfb69cf78010b8bede91f817d5b41e1d2ec2c
YunByungil <dbsquddlfz@naver.com>
2024-01-08 22:40:38 +0900

    [BE-33] feat: 올바르지 않은 확장자 에러 코드 등록

--- src/main/java/com/chatty/constants/Code.java
+++ src/main/java/com/chatty/constants/Code.java
@@ -10,6 +10,7 @@ public enum Code {
 
     OK(HttpStatus.OK, "응답 성공"),
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "올바르지 않은 파라미터 형식입니다."),
+    INVALID_EXTENSION(HttpStatus.BAD_REQUEST, "올바르지 않은 확장자입니다."),
     SMS_NOT_SEND(HttpStatus.BAD_GATEWAY, "sms전송을 실패했습니다."),
     INVALID_AUTH_NUMBER(HttpStatus.BAD_REQUEST, "유효하지 않은 인증 번호 입니다."),
     ALREADY_EXIST_USER(HttpStatus.BAD_REQUEST, "이미 존재 하는 유저 입니다."),
