7886216721eb6db0d61ec155b93951953261af61
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-01-09 20:37:47 +0900

    [BE-54] docs: chat관련 swagger 추가

--- src/main/java/com/chatty/controller/chat/MessageController.java
+++ src/main/java/com/chatty/controller/chat/MessageController.java
@@ -6,6 +6,7 @@ import com.chatty.dto.chat.response.MessageMarkResponseDto;
 import com.chatty.dto.chat.response.MultipleMessageResponseDto;
 import com.chatty.dto.chat.response.SimpleMessageResponseDto;
 import com.chatty.service.chat.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -22,6 +23,7 @@ public class MessageController {
 
     private final ChatService chatService;
 
+    @Operation(summary = "메시지 전송", description = "구독자들에게 메시지를 전송합니다. 경로 맨앞에 pub을 붙여주어야 합니다. ex) /pub/chat/message/{roomId}")
     @MessageMapping(value = "/chat/message/{roomId}")
     @SendTo(value = "/sub/chat/{roomId}")
     public ApiResponse<SimpleMessageResponseDto> message(@DestinationVariable Long roomId, MessageDto messageDto){
@@ -29,12 +31,14 @@ public class MessageController {
         return ApiResponse.ok(chatService.saveMessage(roomId, messageDto));
     }
 
+    @Operation(summary = "메시지 읽음처리", description = "메시지를 읽음 처리합니다.")
     @MessageMapping(value = "/readMessage/{messageId}")
     public ApiResponse<MessageMarkResponseDto> readMessage(@DestinationVariable Long messageId){
         log.info("메시지 읽음 처리 - 아이디 : {}",messageId);
         return ApiResponse.ok(chatService.markMessageAsRead(messageId));
     }
 
+    @Operation(summary = "메시지 가져오기", description = "읽지 않은 메시지를 가져옵니다.")
     @PostMapping("/chat/messages")
     public ApiResponse<MultipleMessageResponseDto> getMessages(@RequestBody UnreadMessageDto unreadMessageDto){
         return ApiResponse.ok(chatService.getMessages(unreadMessageDto));
