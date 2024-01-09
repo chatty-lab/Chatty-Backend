ef632587301d9f9e4339adb407f021283002115b
YunByungil <dbsquddlfz@naver.com>
2024-01-09 17:14:45 +0900

    [BE-33] test: 매칭 성공 시, 상태 false -> true 테스트 완료

--- src/test/java/com/chatty/service/match/MatchServiceTest.java
+++ src/test/java/com/chatty/service/match/MatchServiceTest.java
@@ -2,13 +2,13 @@ package com.chatty.service.match;
 
 import com.chatty.dto.match.request.MatchRequest;
 import com.chatty.dto.match.response.MatchResponse;
+import com.chatty.entity.match.Match;
 import com.chatty.entity.user.Authority;
 import com.chatty.entity.user.Coordinate;
 import com.chatty.entity.user.Gender;
 import com.chatty.entity.user.User;
 import com.chatty.repository.match.MatchRepository;
 import com.chatty.repository.user.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MatchServiceTest {
@@ -64,6 +64,24 @@ class MatchServiceTest {
                 .containsExactlyInAnyOrder("박지성", Gender.MALE, 25, 25, 30);
     }
 
+    @DisplayName("매칭이 완료되었을 때, isSuccess 값이 true로 바뀐다.")
+    @Test
+    void updateIsSuccess() {
+        // given
+        LocalDate birth = LocalDate.of(2000, 1, 1);
+        User user = createUser("박지성", "01022222222", birth);
+        userRepository.save(user);
+
+        Match match = createMatch(user, LocalDateTime.now(), 25, 30, 30.0, Gender.FEMALE);
+        matchRepository.save(match);
+
+        // when
+        MatchResponse matchResponse = matchService.successMatch(match.getId());
+
+        // then
+        assertThat(matchResponse.isSuccess()).isTrue();
+    }
+
     private User createUser(final String nickname, final String mobileNumber, final LocalDate birth) {
         return User.builder()
                 .mobileNumber(mobileNumber)
@@ -81,6 +99,19 @@ class MatchServiceTest {
                                 .lng(127.1)
                                 .build()
                 ))
+                .build();
+    }
+
+    private Match createMatch(final User user, final LocalDateTime localDateTime, final int minAge, final int maxAge, final double scope, final Gender gender) {
+        return Match.builder()
+                .user(user)
+                .minAge(minAge)
+                .maxAge(maxAge)
+                .scope(scope)
+                .gender(gender)
+                .category("category")
+                .registeredDateTime(localDateTime)
+                .isSuccess(false)
                 .build();
     }
 }
