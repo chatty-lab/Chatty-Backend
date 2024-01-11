d343faa46600076bb20d7d3c1f659d060ca5229f
YunByungil <dbsquddlfz@naver.com>
2024-01-11 17:08:54 +0900

    [BE-33] style: 불 필요한 주석 삭제

--- src/main/java/com/chatty/service/match/MatchHandler.java
+++ src/main/java/com/chatty/service/match/MatchHandler.java
@@ -186,88 +186,8 @@ public class MatchHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) throws Exception {
-//        super.afterConnectionClosed(session, status);
         log.info("세션 삭제 완료 소켓 연결 끊음.");
         sessions.remove(session);
     }
 
-//    private void registerClientInfo(WebSocketSession session) {
-//        String query = session.getUri().getQuery();
-//        String[] paramArray = query.split("&");
-//
-//        for (int i = 0; i < 2; i++) {
-//            String param = paramArray[i];
-//            paramArray[i] = param.substring(param.indexOf("=") + 1);
-//        }
-//
-//        String mobileNumber = paramArray[0];
-//        String scope = paramArray[1];
-//
-//        System.out.println("mobileNumber = " + mobileNumber);
-//        System.out.println("scope = " + scope);
-//
-//        User user = userRepository.findUserByMobileNumber(mobileNumber)
-//                .orElseThrow(() -> new CustomException(NOT_EXIST_USER));
-//
-//        Map<String, Object> attributes = session.getAttributes();
-//        attributes.put("mobileNumber", user.getMobileNumber());
-//        attributes.put("nickname", user.getNickname());
-//        attributes.put("gender", user.getGender());
-//        int age = LocalDate.now().getYear() - user.getBirth().getYear() + 1;
-//        attributes.put("age", age);
-//        attributes.put("mbti", user.getMbti());
-//        attributes.put("location", user.getLocation());
-////        attributes.put("imageUrl", user.getImageUrl());
-//
-//        sessions.add(session);
-//
-//        log.info("{} 님이 매칭 대기열에 입장했습니다.", user.getNickname());
-//        log.info("sessionId = {}", session.getId());
-//    }
-
-//    public void connectUser(WebSocketSession session) throws Exception {
-//        for (WebSocketSession connected : sessions) {
-//            String myGender = session.getAttributes().get("gender").toString();
-//            String yourGender = connected.getAttributes().get("gender").toString();
-//
-//            Point myLocation = (Point) session.getAttributes().get("location");
-//            Point yourLocation = (Point) connected.getAttributes().get("location");
-//            if (session == connected) {
-//                continue;
-//            }
-//            // connected 에서 뽑았을 때, 여러가지 경우의 수를 다 따져봐야 한다.
-//            /*
-//            - 세션 정보가 이미 매칭이 되어있는 상태인지 먼저 검증한다.
-//            1. 성별이 다른지
-//            2. 내가 설정한 거리 범위 안에 존재하는지
-//            3. 이미 매칭된 적이 있거나, 차단을 한 유저인지
-//            4. 내가 선택한 관심사를 지니고 있는 유저인지
-//             */
-//            if (connected.getAttributes().containsKey("destination")) {
-//                continue;
-//            }
-//            if (myGender.equals(yourGender)) { // 성별이 같으면 continue
-//                continue;
-//            }
-//            if (userRepository.customFindByDistance2(yourLocation, myLocation, 999.0) == null) {
-//                continue;
-//            }
-//
-////            sendUserInfo(session, connected);
-////            sendUserInfo(connected, session);
-//        }
-//    }
-
-//    private void sendUserInfo(WebSocketSession mySession, WebSocketSession duoSession) throws Exception {
-//        User user = userRepository.findUserByMobileNumber(mySession.getAttributes().get("mobileNumber").toString())
-//                .orElseThrow(() -> new CustomException(NOT_EXIST_USER));
-//
-//        MatchResponse userResponse = MatchResponse.of(user, mySession.getAttributes().get("age").toString());
-//
-//        mySession.getAttributes().put("destination", duoSession.getId());
-//
-//        String json = gson.toJson(userResponse);
-//        TextMessage textMessage = new TextMessage(json);
-//        duoSession.sendMessage(textMessage);
-//    }
 }
