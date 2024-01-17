c8e7cc06dbba795ca8c55e10d97ecc99dda5f331
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-01-17 15:24:09 +0900

    feat: jwtTokenfliter CustomException 적용

---
+++ src/main/java/com/chatty/handler/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,37 @@
+package com.chatty.handler;
+
+import com.chatty.constants.Code;
+import com.chatty.dto.ErrorResponse;
+import com.chatty.exception.CustomException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        setResponse(request,response);
+    }
+
+    private void setResponse(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        CustomException exception = (CustomException) request.getAttribute("errorCode");
+        Code errorCode = exception.getCode();
+        ErrorResponse errorResponse = ErrorResponse.of(errorCode.getErrorCode(), errorCode.getMessage());
+        response.setStatus(Code.INVALID_TOKEN.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
