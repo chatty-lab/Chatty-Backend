a1d6541b65e753bf1c68b760c50539d22e94e90a
YunByungil <dbsquddlfz@naver.com>
2024-01-23 14:37:50 +0900

    [BE-55] feat: User Entity에 blueCheck 컬럼 추가

--- src/main/java/com/chatty/entity/user/User.java
+++ src/main/java/com/chatty/entity/user/User.java
@@ -67,6 +67,8 @@ public class User  extends CommonEntity implements UserDetails{
 
     private String deviceToken;
 
+    private boolean blueCheck;
+
     public void joinComplete(final User request) {
         this.nickname = request.getNickname();
         this.location = request.getLocation();
