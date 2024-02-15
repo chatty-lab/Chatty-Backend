3799a0d16456f86c0a171e033f4dda8cb76d6c4d
YunByungil <dbsquddlfz@naver.com>
2024-02-15 13:41:18 +0900

    [BE-73] test: 상대방 프로필 잠금 해제 Controller 및 Repo 메서드 테스트 완료
    - Controller Validation 값 테스트
    - existsByUnlockerIdAndUnlockedUserId 메서드 테스트 완료

---
+++ src/test/java/com/chatty/controller/profile/ProfileUnlockControllerTest.java
@@ -0,0 +1,84 @@
+package com.chatty.controller.profile;
+
+import com.chatty.dto.profileUnlock.request.ProfileUnlockRequest;
+import com.chatty.service.profileUnlock.ProfileUnlockService;
+import com.chatty.service.user.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ProfileUnlockController.class)
+class ProfileUnlockControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private ProfileUnlockService profileUnlockService;
+
+    @BeforeEach
+    void setUp() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(new UsernamePasswordAuthenticationToken("01012345678", "", List.of()));
+    }
+
+    @DisplayName("상대방 프로필 잠금을 해제한다.")
+    @Test
+    void unlockProfile() throws Exception {
+        // given
+        ProfileUnlockRequest request = ProfileUnlockRequest.builder()
+                .unlock(true)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/v1/users/profile/{userId}", 2L).with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("상대방 프로필 잠금을 해제할 때, true값을 입력해야 된다.")
+    @Test
+    void unlockProfileWithFalse() throws Exception {
+        // given
+        ProfileUnlockRequest request = ProfileUnlockRequest.builder()
+                .unlock(false)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        post("/api/v1/users/profile/{userId}", 2L).with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("true 값만 들어올 수 있습니다."));
+    }
+}
---
+++ src/test/java/com/chatty/repository/profileUnlock/ProfileUnlockRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.chatty.repository.profileUnlock;
+
+import com.chatty.constants.Authority;
+import com.chatty.entity.user.Gender;
+import com.chatty.entity.user.ProfileUnlock;
+import com.chatty.entity.user.User;
+import com.chatty.repository.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class ProfileUnlockRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProfileUnlockRepository profileUnlockRepository;
+
+    @DisplayName("ProfileUnlock 엔티티에서 1번 유저가 2번 유저를 잠금 해제했는지 확인한다.")
+    @Test
+    void existsByUnlockerIdAndUnlockedUserId() {
+        // given
+        User user1 = createUser("박지성", "01012345678", Gender.MALE);
+        User user2 = createUser("김연아", "01099998888", Gender.FEMALE);
+        userRepository.saveAll(List.of(user1, user2));
+
+        LocalDateTime now = LocalDateTime.now();
+        ProfileUnlock profileUnlock = ProfileUnlock.builder()
+                .unlocker(user1)
+                .unlockedUser(user2)
+                .localDateTime(now)
+                .build();
+
+        profileUnlockRepository.save(profileUnlock);
+
+        // when
+        boolean result = profileUnlockRepository.existsByUnlockerIdAndUnlockedUserId(user1.getId(), user2.getId());
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    private User createUser(final String nickname, final String mobileNumber, final Gender gender) {
+        return User.builder()
+                .mobileNumber(mobileNumber)
+                .deviceId("123123")
+                .authority(Authority.USER)
+                .deviceToken("123123")
+                .birth(LocalDate.now())
+                .imageUrl("이미지")
+                .address("주소")
+                .gender(gender)
+                .nickname(nickname)
+                .build();
+    }
+
+}
