c7107f6bc266a7bd946cd4176a8c1cd9b90331fb
YunByungil <dbsquddlfz@naver.com>
2024-02-22 19:02:11 +0900

    [BE-76] docs: 상대방 프로필 조회 및 잠금 swagger-ui 작성

--- src/main/java/com/chatty/controller/profile/ProfileUnlockController.java
+++ src/main/java/com/chatty/controller/profile/ProfileUnlockController.java
@@ -5,6 +5,7 @@ import com.chatty.dto.profileUnlock.request.ProfileUnlockRequest;
 import com.chatty.dto.profileUnlock.response.ProfileUnlockResponse;
 import com.chatty.dto.user.response.UserProfileResponse;
 import com.chatty.service.profileUnlock.ProfileUnlockService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +22,14 @@ public class ProfileUnlockController {
 
     private final ProfileUnlockService profileUnlockService;
 
+    @Operation(summary = "프로필 잠금 해제", description = "상대방 프로필 잠금을 해제합니다.")
     @PostMapping("/profile/{userId}")
     public ApiResponse<ProfileUnlockResponse> unlockProfile(@PathVariable final Long userId, Authentication authentication,
                                                              @Valid @RequestBody final ProfileUnlockRequest request) {
         return ApiResponse.ok(profileUnlockService.unlockProfile(userId, authentication.getName(), request, LocalDateTime.now()));
     }
 
+    @Operation(summary = "상대방 프로필 조회", description = "상대방 프로필을 조회합니다. 잠금을 해제하였으면 true, 아니면 false")
     @GetMapping("/profile/{userId}")
     public ApiResponse<UserProfileResponse> getUserProfile(@PathVariable final Long userId, Authentication authentication) {
         return ApiResponse.ok(profileUnlockService.getUserProfile(userId, authentication.getName()));
