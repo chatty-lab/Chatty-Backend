5ac1fe368060692921ea9e2e3f93ba896b800995
YunByungil <dbsquddlfz@naver.com>
2024-02-22 16:21:07 +0900

    [BE-76] test: User Entity candy 감소 메서드 및 예외 체크 로직 테스트

--- src/test/java/com/chatty/entity/user/UserTest.java
+++ src/test/java/com/chatty/entity/user/UserTest.java
@@ -1,5 +1,6 @@
 package com.chatty.entity.user;
 
+import com.chatty.exception.CustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,12 +19,58 @@ class UserTest {
 
         // then
         assertThat(user.getNickname()).isEqualTo("닉네임수정");
+    }
+    
+    @DisplayName("내가 보유하고있는 캔디의 수량이 필요한 캔디의 수량보다 작은지 확인한다.")
+    @Test
+    void isCandyQuantityLessThan() {
+        // given
+        User user = createUser(6);
 
+        // when
+        boolean result = user.isCandyQuantityLessThan(7);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("내가 보유하고있는 캔디의 수량을 차감한다.")
+    @Test
+    void deductCandyQuantity() {
+        // given
+        User user = createUser(7);
+        int candy = 7;
+
+        // when
+        user.deductCandyQuantity(candy);
+
+        // then
+        assertThat(user.getCandy()).isZero();
+    }
+
+    @DisplayName("내가 보유하고있는 캔디의 수량이 부족하면 예외가 발생한다.")
+    @Test
+    void deductCandyQuantity2() {
+        // given
+        User user = createUser(6);
+        int candy = 7;
+
+        // when // then
+        assertThatThrownBy(() -> user.deductCandyQuantity(candy))
+                .isInstanceOf(CustomException.class)
+                .hasMessage("캔디의 개수가 부족합니다.");
     }
 
     private User createUser() {
         return User.builder()
                 .nickname("닉네임")
+                .build();
+    }
+
+    private User createUser(final int candy) {
+        return User.builder()
+                .nickname("닉네임")
+                .candy(candy)
                 .build();
     }
 }
