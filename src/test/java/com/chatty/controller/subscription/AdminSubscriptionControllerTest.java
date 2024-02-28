a18e68b51787de9cd1329fa65289970a4ce17721
YunByungil <dbsquddlfz@naver.com>
2024-02-28 15:43:12 +0900

    [BE-78] test: Admin 구독권 등록 수정 Validation 테스트 완료

--- src/test/java/com/chatty/controller/subscription/AdminSubscriptionControllerTest.java
+++ src/test/java/com/chatty/controller/subscription/AdminSubscriptionControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdminSubscriptionController.class)
@@ -51,6 +52,75 @@ class AdminSubscriptionControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("ADMIN 권한이 새로운 구독권을 등록할 때, 구독권 이름은 필수값이다.")
+    @WithMockUser(username = "01012345678", roles = "ADMIN")
+    @Test
+    void createSubscriptionWithoutName() throws Exception {
+        // given
+        SubscriptionCreateRequest request = SubscriptionCreateRequest.builder()
+//                .name("구독권1")
+                .duration(30)
+                .price(9_900)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        post("/v1/subscription").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value("E000"))
+                .andExpect(jsonPath("$.message").value("구독권 이름은 필수로 입력해야 합니다."));
+    }
+
+    @DisplayName("ADMIN 권한이 새로운 구독권을 등록할 때, 구독권 기간은 최소 30일 이상이다.")
+    @WithMockUser(username = "01012345678", roles = "ADMIN")
+    @Test
+    void createSubscriptionWithThirtyDayDuration() throws Exception {
+        // given
+        SubscriptionCreateRequest request = SubscriptionCreateRequest.builder()
+                .name("구독권1")
+                .duration(29)
+                .price(9_900)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        post("/v1/subscription").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value("E000"))
+                .andExpect(jsonPath("$.message").value("구독권 기간은 30일 이상이여야 합니다."));
+    }
+
+    @DisplayName("ADMIN 권한이 새로운 구독권을 등록할 때, 구독권 가격은 양수이다.")
+    @WithMockUser(username = "01012345678", roles = "ADMIN")
+    @Test
+    void createSubscriptionWithZeroPrice() throws Exception {
+        // given
+        SubscriptionCreateRequest request = SubscriptionCreateRequest.builder()
+                .name("구독권1")
+                .duration(30)
+                .price(0)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        post("/v1/subscription").with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value("E000"))
+                .andExpect(jsonPath("$.message").value("구독권 가격은 양수여야 합니다."));
     }
 
     @DisplayName("ADMIN 권한이 아닌 사람이 새로운 구독권을 등록하면 403예외가 발생한다.")
@@ -93,6 +163,75 @@ class AdminSubscriptionControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @DisplayName("구독권을 수정할 때, 구독권 이름은 필수값이다.")
+    @WithMockUser(username = "01012345678", roles = "ADMIN")
+    @Test
+    void updateSubscriptionWithoutName() throws Exception {
+        // given
+        SubscriptionCreateRequest request = SubscriptionCreateRequest.builder()
+//                .name("구독권1")
+                .duration(30)
+                .price(9_900)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        put("/v1/subscription/{subscriptionId}", 1L).with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value("E000"))
+                .andExpect(jsonPath("$.message").value("구독권 이름은 필수로 입력해야 합니다."));
+    }
+
+    @DisplayName("ADMIN 권한이 새로운 구독권을 등록할 때, 구독권 기간은 최소 30일 이상이다.")
+    @WithMockUser(username = "01012345678", roles = "ADMIN")
+    @Test
+    void updateSubscriptionWithLessThanThirtyDayDuration() throws Exception {
+        // given
+        SubscriptionCreateRequest request = SubscriptionCreateRequest.builder()
+                .name("구독권1")
+                .duration(29)
+                .price(9_900)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        put("/v1/subscription/{subscriptionId}", 1L).with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value("E000"))
+                .andExpect(jsonPath("$.message").value("구독권 기간은 30일 이상이여야 합니다."));
+    }
+
+    @DisplayName("ADMIN 권한이 새로운 구독권을 등록할 때, 구독권 가격은 양수이다.")
+    @WithMockUser(username = "01012345678", roles = "ADMIN")
+    @Test
+    void updateSubscriptionWithZeroPrice() throws Exception {
+        // given
+        SubscriptionCreateRequest request = SubscriptionCreateRequest.builder()
+                .name("구독권1")
+                .duration(30)
+                .price(0)
+                .build();
+
+        // when // then
+        mockMvc.perform(
+                        put("/v1/subscription/{subscriptionId}", 1L).with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errorCode").value("E000"))
+                .andExpect(jsonPath("$.message").value("구독권 가격은 양수여야 합니다."));
     }
 
     @DisplayName("ADMIN 권한이 아닌 사람이 등록되어있는 구독권을 수정할 때, 403예외가 발생한다.")
