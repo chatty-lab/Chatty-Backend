13327858659a1f2735148745bf067bda402509be
YunByungil <dbsquddlfz@naver.com>
2024-02-28 15:42:46 +0900

    [BE-78] feat: Admin 구독권 등록 수정 @Valid 설정

--- src/main/java/com/chatty/dto/subscription/request/SubscriptionUpdateRequest.java
+++ src/main/java/com/chatty/dto/subscription/request/SubscriptionUpdateRequest.java
@@ -1,6 +1,9 @@
 package com.chatty.dto.subscription.request;
 
 import com.chatty.entity.subscription.Subscription;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,10 +13,13 @@ import lombok.NoArgsConstructor;
 @Getter
 public class SubscriptionUpdateRequest {
 
+    @NotBlank(message = "구독권 이름은 필수로 입력해야 합니다.")
     private String name;
 
+    @Min(value = 30, message = "구독권 기간은 30일 이상이여야 합니다.")
     private int duration;
 
+    @Positive(message = "구독권 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
