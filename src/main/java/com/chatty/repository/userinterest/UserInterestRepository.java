38f568bf55e2ed7fa1d88364ea9ee24fb544309b
YunByungil <dbsquddlfz@naver.com>
2024-03-02 17:06:29 +0900

    feat: updateInterests 메서드 테스트를 위한 Repo 생성
    - fk로 묶여있기 때문에 BeforeEach에서 deleteAll 이 필요하다

---
+++ src/main/java/com/chatty/repository/userinterest/UserInterestRepository.java
@@ -0,0 +1,7 @@
+package com.chatty.repository.userinterest;
+
+import com.chatty.entity.user.UserInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
+}
