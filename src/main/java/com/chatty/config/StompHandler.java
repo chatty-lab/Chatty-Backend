d60e3e4ebaedd473e804f1f7c513b6a01e4b886f
YunByungil <dbsquddlfz@naver.com>
2024-03-12 16:28:48 +0900

    feat: 스톰프 인터셉터 제거

--- src/main/java/com/chatty/config/StompHandler.java
+++ src/main/java/com/chatty/config/StompHandler.java
@@ -2,6 +2,7 @@ package com.chatty.config;
 
 import com.chatty.validator.TokenValidator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class StompHandler implements ChannelInterceptor {
@@ -20,9 +22,11 @@ public class StompHandler implements ChannelInterceptor {
     @Override
     public Message<?> preSend(final Message<?> message, final MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        log.info("PreSend = gdgd");
         System.out.println("message:" + message);
         System.out.println("헤더 : " + message.getHeaders());
         System.out.println("토큰" + accessor.getNativeHeader("Authorization"));
+        System.out.println("accessor.getFirstNativeHeader(\"Authorization\") = " + accessor.getFirstNativeHeader("Authorization"));
         // apic 이랑 websocket 테스트 툴이랑 다름.
 //        if (StompCommand.CONNECT.equals(accessor.getCommand()) || StompCommand.SEND.equals(accessor.getCommand())) {
 //            System.out.println("검증을 제대로 하나요?");
